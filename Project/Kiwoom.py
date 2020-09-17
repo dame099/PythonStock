97d31d0823414f0afee1aaa6cb4638ff46fddacd
dame0 <sejune99@gmail.com>
2020-09-17 13:25:37 +0900

    9/15

--- Project/Kiwoom.py
+++ Project/Kiwoom.py
@@ -8,6 +8,8 @@ import sqlite3
 
 TR_REQ_TIME_INTERVAL = 0.2
 
+#모의투자 계좌번호 8145067911
+
 ###################################################################
 #Kiwoom 객체
 #OpenAPI+와 통신을 쉽게 할 수 있도록 만드는 객체
@@ -18,6 +20,7 @@ class Kiwoom(QAxWidget):    #OpenAPI+의 메서드를 호출하기 위해서는 
         self._create_kiwoom_instance()
         self._set_signal_slots()
 
+
     #OpenAPI+에 대한 객체 생성 함수
     def _create_kiwoom_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")    #COM오브젝트 생성
@@ -26,6 +29,7 @@ class Kiwoom(QAxWidget):    #OpenAPI+의 메서드를 호출하기 위해서는 
     def _set_signal_slots(self):
         self.OnEventConnect.connect(self._event_connect)
         self.OnReceiveTrData.connect(self._receive_tr_data)
+        self.OnReceiveChejanData.connect(self._receive_chejan_data)
 
     #로그인을 위한 함수
     def comm_connect(self):
@@ -49,8 +53,9 @@ class Kiwoom(QAxWidget):    #OpenAPI+의 메서드를 호출하기 위해서는 
         code_list = code_list.split(';')
         return code_list[:-1]
 
-    def get_master_code_name(self,code):
-        code_name = self.dynamicCall("GetMasterCodeName(QString)",code)
+    #종목코드에 해당하는 종목명을 반환하는 함수
+    def get_master_code_name(self, code):
+        code_name = self.dynamicCall("GetMasterCodeName(QString)", code)
         return code_name
 
     #연결상태를 확인해주는 함수
@@ -91,6 +96,12 @@ class Kiwoom(QAxWidget):    #OpenAPI+의 메서드를 호출하기 위해서는 
         if rqname == 'opt10081_req':   #rqname이 opt10081_req이면 해당 메소드를 호출
             self._opt10081(rqname,trcode)
 
+        elif rqname == "opw00001_req":
+            self._opw00001(rqname, trcode)
+
+        elif rqname == "opw00018_req":
+            self._opw00018(rqname, trcode)
+
         try:
             self.tr_event_loop.exit()   #comm_rq_data에서 시작한 이벤트 루프 종료
         except AttributeError:
@@ -107,23 +118,145 @@ class Kiwoom(QAxWidget):    #OpenAPI+의 메서드를 호출하기 위해서는 
             low = self._comm_get_data(trcode, "", rqname, i, "저가")
             close = self._comm_get_data(trcode, "", rqname, i, "현재가")
             volume = self._comm_get_data(trcode, "", rqname, i, "거래량")
-            
-            
+
+            self.ohlcv['date'].append(date)
+            self.ohlcv['open'].append(int(open))
+            self.ohlcv['high'].append(int(high))
+            self.ohlcv['low'].append(int(low))
+            self.ohlcv['close'].append(int(close))
+            self.ohlcv['volume'].append(int(volume))
+
+
+    #주식 주문을 보내는 함수    
+    def send_order(self,rqname,screen_no,acc_no,order_type,code,quantity,price,hoga,order_no):
+        print(code)
+        self.dynamicCall("SendOrder(QString,QString,QString,int,QString,int,int,QString,QString)",[rqname,screen_no,acc_no,order_type,code,quantity,price,hoga,order_no])    
+
+    #체결 잔고를 받아오는 함수, OnReceiveChejan이벤트가 호출될 때 그 안에서 사용해야 한다. 
+    def get_chejan_data(self,fid):
+        ret = self.dynamicCall("GetChejanData(int),fid")
+        return ret
+
+    #OnReceiveChejan이벤트를 처리해주는 함수
+    def _receive_chejan_data(self,gubun,itme_cnt,fid_list):
+        print(gubun)    #체결구분 접수와 체결시 '0'값, 국내주식 잔고전달은 '1'값, 파생잔고 전달은 '4'
+        #fid값을 통해 서로 다른 데이터를 얻을 수 있다.
+        print(self.get_chejan_data(9023))   #fid=9023, 주문번호
+        print(self.get_chejan_data(302))    #fid=302, 종목명
+        print(self.get_chejan_data(900))    #fid=900, 주문수량
+        print(self.get_chejan_data(901))    #fid=901, 주문가격
+
+    #로그인 사용자 정보를 얻어오는 함수
+    def get_login_info(self,tag):
+        ret = self.dynamicCall("GetLoginInfo(QString)",tag)
+        return ret
+
+    #계좌의 예수금(d+2)정보를 얻어오는 함수
+    def _opw00001(self, rqname, trcode):
+        d2_deposit = self._comm_get_data(trcode, "", rqname, 0, "d+2추정예수금")
+        self.d2_deposit = Kiwoom.change_format(d2_deposit)
+
+    #계좌의 평가잔고 데이터 및 보유 종목별 데이터를 읽어오는 함수
+    def _opw00018(self,rqname,trcode):
+        #싱글데이터를 통해 계좌 잔고관련 정보를 얻어오는 부분
+        total_purchase_price = self._comm_get_data(trcode,"",rqname,0,"총매입금액")
+        total_eval_price = self._comm_get_data(trcode, "", rqname, 0, "총평가금액")
+        total_eval_profit_loss_price = self._comm_get_data(trcode, "", rqname, 0, "총평가손익금액")
+        total_earning_rate = self._comm_get_data(trcode, "", rqname, 0, "총수익률(%)")
+        estimated_deposit = self._comm_get_data(trcode, "", rqname, 0, "추정예탁자산")
+
+        #모의투자인 경우에는 총수익률 / 100을 해주어야 한다.
+        if not self.get_server_gubun():
+            total_earning_rate = float(total_earning_rate) / 100
+            total_earning_rate = str(total_earning_rate)
+
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_purchase_price))
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_eval_price))
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_eval_profit_loss_price))
+        self.opw00018_output['single'].append(Kiwoom.change_format(total_earning_rate))
+        self.opw00018_output['single'].append(Kiwoom.change_format(estimated_deposit))
+
+        #멀티데이터를 통해 보유종목 관련 데이터를 얻어오는 부분
+        rows = self._get_repeat_cnt(trcode,rqname)
+        #한번에 20개 종목만 조회 가능하므로 반복문을 통해 구현
+        for i in range(rows):   
+            name = self._comm_get_data(trcode,"",rqname,i,"종목명")
+            quantity = self._comm_get_data(trcode, "", rqname, i, "보유수량")
+            purchase_price = self._comm_get_data(trcode, "", rqname, i, "매입가")
+            current_price = self._comm_get_data(trcode, "", rqname, i, "현재가")
+            eval_profit_loss_price = self._comm_get_data(trcode, "", rqname, i, "평가손익")
+            earning_rate = self._comm_get_data(trcode, "", rqname, i, "수익률(%)")
+
+            print(name)
+            quantity = Kiwoom.change_format(quantity)
+            purchase_price = Kiwoom.change_format(purchase_price)
+            current_price = Kiwoom.change_format(current_price)
+            eval_profit_loss_price = Kiwoom.change_format(eval_profit_loss_price)
+            earning_rate = Kiwoom.change_format2(earning_rate)  #수익률은 change_format2함수를 통해 포맷 변경해준다.
+
+            self.opw00018_output['multi'].append([name, quantity, purchase_price, current_price,eval_profit_loss_price, earning_rate])
+    
+    def reset_opw00018_output(self):
+        self.opw00018_output = {'single':[], 'multi':[]}
+
+    #실서버와 모의투자 서버를 구분해주는 함수
+    def get_server_gubun(self):
+        ret = self.dynamicCall("KOA_Functions(QString, QString)", "GetServerGubun", "")
+        return ret
+
+########################
+    #정적메서드 구현
+########################
+    #큰 숫자가 들어왔을때 가독성을 위해 쉼표를 표시해주는 함수
+    @staticmethod 
+    def change_format(data):
+        strip_data = data.lstrip('-0')  #문자열의 왼쪽에 존재하는 '-' 또는 '0'을 제거
+        if strip_data =='':
+            strip_data = '0'    #0이 입력으로 들어온 경우
+
+        #천의 자리마다 콤마를 추가, 책에서 긁어온거라 잘 모름.
+        try:
+            format_data = format(int(strip_data), ',d')
+        except:
+            format_data = format(float(strip_data))
+
+        if data.startswith('-'):
+            format_data = '-' + format_data
+
+        return format_data   
+    def change_format2(data):
+        strip_data = data.lstrip('-0')
+
+        if strip_data == '':
+            strip_data = '0'
+
+        if strip_data.startswith('.'):
+            strip_data = '0' + strip_data
+
+        if data.startswith('-'):
+            strip_data = '-' + strip_data
+
+        return strip_data
+
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     kiwoom = Kiwoom()
     kiwoom.comm_connect()
 
-    # opt10081 TR 요청
-    kiwoom.set_input_value("종목코드", "039490")
-    kiwoom.set_input_value("기준일자", "20200910")
-    kiwoom.set_input_value("수정주가구분", 1)
-    kiwoom.comm_rq_data("opt10081_req", "opt10081", 0, "0101")
+    accouns_num = int(kiwoom.get_login_info("ACCOUNT_CNT"))
+    accounts = kiwoom.get_login_info("ACCNO")
 
-    while kiwoom.remained_data == True:
-        time.sleep(TR_REQ_TIME_INTERVAL)
-        kiwoom.set_input_value("종목코드", "039490")
-        kiwoom.set_input_value("기준일자", "20170224")
-        kiwoom.set_input_value("수정주가구분", 1)
-        kiwoom.comm_rq_data("opt10081_req", "opt10081", 2, "0101")
+    #얻어온 계좌를 출력
+    accounts_list = accounts.split(';')[0:accouns_num]
+    print(accounts_list)
+
+    kiwoom.set_input_value("계좌번호",accounts_list[0])
+    kiwoom.set_input_value("비밀번호","1152")
+    kiwoom.comm_rq_data("opw00001_req","opw00001",0,"2000")
+    print(kiwoom.d2_deposit)
+
+    kiwoom.set_input_value("계좌번호",accounts_list[0])
+    kiwoom.comm_rq_data("opw00018_req","opw00018",0,"2000")
--- Project/PyTrader.py
+++ Project/PyTrader.py
@@ -18,6 +18,33 @@ class MyWindow(QMainWindow,form_class):
         self.timer.start(1000)  #1초
         self.timer.timeout.connect(self.timeout)    #1초마다 이벤트 발생
 
+        self.lineEdit.textChanged.connect(self.code_changed)    #lineEdit에 변화가 생기면 code_changed메서드로 연결
+        #키움 모듈을 사용해서 계좌수와 계좌 번호를 받아옴.
+        accouns_num = int(self.kiwoom.get_login_info("ACCOUNT_CNT"))
+        accounts = self.kiwoom.get_login_info("ACCNO")
+
+        #얻어온 계좌를 QCombox에 출력
+        accounts_list = accounts.split(';')[0:accouns_num]
+        print(accounts_list)
+        self.comboBox.addItems(accounts_list)   #combox에 읽어온 계좌 입력
+
+        #종목코드가 입력되면 해당 종목명을 출력
+        self.lineEdit.textChanged.connect(self.code_changed)
+
+        #현금매수 버튼을 누루면 주문이 이뤄지도록 구현
+        self.pushButton.clicked.connect(self.send_order)
+
+        #조회버튼의 기능 구현
+        self.pushButton_2.clicked.connect(self.check_balance)
+
+        # Timer2, 10초마다 실시간 조회를 위한 타이머
+        self.timer2 = QTimer(self)
+        self.timer2.start(1000*10)
+        self.timer2.timeout.connect(self.timeout2)
+
+
+
+
     def timeout(self):
         current_time = QTime.currentTime()
         text_time = current_time.toString("hh:mm:ss")
@@ -31,7 +58,78 @@ class MyWindow(QMainWindow,form_class):
 
         self.statusbar.showMessage(state_msg + " | " + time_msg)
 
+    #종목코드 입력칸(lineEdit)에 값이 입력되면, API를 이용해서 해당 종목명을 lindEdit_2에 출력해주는 기능
+    def code_changed(self):
+        code = self.lineEdit.text()
+        name = self.kiwoom.get_master_code_name(code)
+        self.lineEdit_2.setText(name)
 
+
+    #주문을 전달하는 함수
+    def send_order(self):
+        order_type_looup = {'신규매수' : 1, '신규매도' : 2, '매수취소' : 3, '매도취소' : 4}
+        hoga_lookup = {'지정가' : '00', "시장가" : '03'}
+
+        account = self.comboBox.currentText()
+        order_type = self.comboBox_2.currentText()
+        code = self.lineEdit.text()
+        hoga = self.comboBox_3.currentText()
+        num = self.spinBox.value()
+        price = self.spinBox_2.value()
+
+        self.kiwoom.send_order("send_order_req","0101",account,order_type_looup[order_type],code,num,price,hoga_lookup[hoga],"")
+
+    def check_balance(self):
+        self.kiwoom.reset_opw00018_output()
+        account_number = self.kiwoom.get_login_info("ACCNO")
+        account_number = account_number.split(';')[0]
+
+        #계좌의 평가잔고 및 보유종목을 조회하는 부분
+        self.kiwoom.set_input_value("계좌번호",account_number)
+        self.kiwoom.comm_rq_data("opw00018_req","opw00018",0,"2000")
+        #최대 20개 종목만 조회 가능하므로 그 이상이면 루프를 돈다.
+        while self.kiwoom.remained_data:
+            time.sleep(0.2)
+            self.kiwoom.set_input_value("계좌번호", account_number)
+            self.kiwoom.comm_rq_data("opw00018_req", "opw00018", 2, "2000")
+
+        #계좌의 예수금을 조회하는 부분
+        self.kiwoom.set_input_value("계좌번호",account_number)
+        self.kiwoom.comm_rq_data("opw00001_req","opw00001",0,"2000")
+
+        #조회한 예수금을 QTableWidget에 출력
+        item = QTableWidgetItem(self.kiwoom.d2_deposit)
+        item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+        self.tableWidget.setItem(0, 0, item)
+
+        #조회한 계좌 데이터를 QTableWidget에 출력
+        for i in range(1,6):
+            item = QTableWidgetItem(self.kiwoom.opw00018_output['single'][i - 1])
+            item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+            self.tableWidget.setItem(0, i, item)
+        #아이템의 크기에 맞춰 행의 높이를 조절
+        self.tableWidget.resizeRowsToContents()
+
+        #보유 종목별 데이터를 출력, 코드 통째로 긁어온거라 잘 모름, 수정XXXXX
+        # Item list
+        item_count = len(self.kiwoom.opw00018_output['multi'])
+        print(self.kiwoom.opw00018_output['multi'])
+        self.tableWidget_2.setRowCount(item_count)
+        for j in range(item_count):
+            row = self.kiwoom.opw00018_output['multi'][j]
+            for i in range(len(row)):
+                item = QTableWidgetItem(row[i])
+                item.setTextAlignment(Qt.AlignVCenter | Qt.AlignRight)
+                self.tableWidget_2.setItem(j, i, item)
+
+        self.tableWidget_2.resizeRowsToContents()
+
+
+    def timeout2(self):
+        if self.checkBox.isChecked():
+            self.check_balance()
+
+            
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     myWindow = MyWindow()
